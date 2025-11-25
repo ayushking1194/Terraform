b7dd0a255ccb6c1776a732d0da496b5a4ce622b4
Ayush Srivastava <ayush.srivastava@nutanix.com>
2025-11-26 05:02:58 +0530

    Parameterize VM configuration in main.tf

--- Nutanix/VM/main.tf
+++ Nutanix/VM/main.tf
@@ -1,16 +1,16 @@
 data "nutanix_clusters" "clusters" {}
 
 resource "nutanix_virtual_machine" "vm1" {
-  name         = "test-dou"
+  name         = "<VM_NAME>"
   cluster_uuid = data.nutanix_clusters.clusters.entities.0.metadata.uuid
 
   categories {
-    name  = "Environment"
-    value = "Staging"
+    name  = "<CATEGORY_NAME>"
+    value = "CATEGORY_VALUE"
   }
 
-  num_vcpus_per_socket = 1
-  num_sockets          = 1
-  memory_size_mib      = 2048
+  num_vcpus_per_socket = <int>     # 1,2,3
+  num_sockets          = <int>     # 1,2,3
+  memory_size_mib      = <int_mib> # 1024,2048,4096,8124
 }
 
